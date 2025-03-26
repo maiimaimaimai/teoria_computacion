6595b84a4ea7f849fb2d7b3e7283db1f87e0ac86
maiimaimaimai <mainoicm@gmail.com>
2025-03-26 11:25:52 -0300

    practico0 done

--- practico0/Practico0.hs
+++ practico0/Practico0.hs
@@ -3,20 +3,21 @@ module Practico0 where
 import Data.List
 import Data.Maybe
 
-data Exp = Var X | Emp | Uni Z | Bel Z Exp | Join Exp Exp | Inter Exp Exp | Diff Exp Exp | Incl Exp Exp | Asig X Exp
-data Val = IntList [Int] | BoolVal Bool
-    deriving (Show)
--- data E = []
+data Exp = Var X | Emp | Uni Z | Bel Z Exp | Join Exp Exp | Inter Exp Exp | Diff Exp Exp | Incl Exp Exp | Asig X Exp | Pow Exp | Equ Exp Exp
 
-type Mem = [(X, Val)]
+data Val = IntList [Int] | BoolVal Bool | ListIntList [[Int]] deriving Show 
+
+type Mem = [(X,Val)]
+
 type X = String
+
 type Z = Int
 
 conj1 :: Exp
-conj1 =  Join (Join (Uni 1) (Uni 2)) (Uni 3)
+conj1 = Join (Join (Uni 1) (Uni 2)) (Uni 3)
 
 conj2 :: Exp
-conj2 =  Join (Join (Uni 2) (Uni 3)) (Uni 4)
+conj2 = Join (Join (Uni 2) (Uni 3)) (Uni 4)
 
 conj3 :: Exp
 conj3 = Join conj1 conj2
@@ -40,52 +41,110 @@ incl3 :: Exp
 incl3 = Incl conj1 conj3
 
 ass1 :: Exp
-ass1 = Asig "w" conj1
+ass1 = Asig ("w") conj1
 
 ass2 :: Exp
-ass2 = Asig "x" conj4
+ass2 = Asig ("x") conj4
 
 ass3 :: Exp
-ass3 = Asig "y" pert2
+ass3 = Asig ("y") pert2
 
 ass4 :: Exp
-ass4 = Asig "z" incl2
+ass4 = Asig ("z") incl2
 
-eval :: Exp -> Mem -> Val
-eval Emp _ = IntList []
-eval (Uni z) _ = IntList [z]
-eval (Join e1 e2) m = let IntList s1 = eval e1 m
-                           IntList s2 = eval e2 m
-                       in IntList (nub (s1 ++ s2)) --nub elimina duplicados 
-eval (Inter e1 e2) m = let IntList s1 = eval e1 m
-                            IntList s2 = eval e2 m
-                        in IntList (filter (`elem` s2) s1) --filter filtra los elementos de s1 que están en s2
-eval (Diff e1 e2) m = let IntList s1 = eval e1 m
-                           IntList s2 = eval e2 m
-                       in IntList (filter (`notElem` s2) s1) --filter (notelem) filtra los elementos de s1 que no están en s2
-eval (Bel z e) m = let IntList s = eval e m
-                    in BoolVal (z `elem` s) --devuelve bool si z está en s
-eval (Incl e1 e2) m = let IntList s1 = eval e1 m
-                           IntList s2 = eval e2 m
-                       in BoolVal (all (`elem` s2) s1) --all para verificar si todos los elementos de s1 están en s2
-eval (Asig x e) m = eval e m
-eval (Var x) m = fromMaybe (IntList []) (lkup x m) --busca una variable x en la memoria m
-                                                        --si x está en la memoria, devuelve su valor
-                                                        --si x no existe, devuelve el conjunto vacío []
-eval (Pow e) m = let IntList s = eval e m
-                  in IntList (map length (subsequences s))
-eval (Equal e1 e2) m = let IntList s1 = eval e1 m
-                            IntList s2 = eval e2 m
-                        in BoolVal (nub s1 == nub s2)
-
-lkup :: X -> Mem -> Maybe Val
+lkup :: Eq X => X -> Mem -> Maybe Val 
 lkup k [] = Nothing
 lkup k ((k1,v1):kvs)
     | k == k1 = Just v1
     | otherwise = lkup k kvs
 
-upd :: X -> Val -> Mem -> Mem
+upd :: Eq X => X -> Val -> Mem -> Mem
 upd k v [] = [(k,v)]
 upd k v ((k1,v1):kvs)
     | k == k1 = ((k1,v):kvs)
     | otherwise = ((k1,v1):(upd k v kvs))
+
+belongs :: Int -> [Int] -> Bool
+belongs n [] = False
+belongs n (x:xs)
+    | n == x = True
+    | otherwise = belongs n (xs)
+
+unioni :: [Int] -> [Int] -> [Int]
+unioni [] n = n
+unioni (x:xs) n = (x:(unioni xs n))
+
+intersection :: [Int] -> [Int] -> [Int]
+intersection [] _ = []
+intersection _ [] = []
+intersection (x:xs) (y:ys) 
+    | x == y = (x:(intersection xs ys))
+    | otherwise = ((intersection (x:xs) ys) ++ (intersection xs (y:ys)))
+
+difference :: [Int] -> [Int] -> [Int]
+difference [] n = []
+difference n [] = n
+difference (x:xs) (y:ys)
+    | x == y = (difference xs ys)
+    | otherwise = x:(difference xs ys)
+
+included :: [Int] -> [Int] -> Bool
+included [] _ = True
+included x [] = False
+included (x:xs) (y:ys) 
+    | x == y = included xs (y:ys)
+    | otherwise = included (x:xs) ys && included xs (y:ys)
+
+power :: [Int] -> [[Int]]
+power [] = [[]]
+power (x:xs) = 
+    let ps = power xs 
+    in (map (x:) ps) ++ ps
+
+igual :: Ord Int => [Int] -> [Int] -> Bool
+igual x y = nub (sort x) == nub (sort y)
+
+eval :: Mem -> Exp -> (Mem, Val)
+eval m (Var x) = 
+    let Just b = lkup x m
+    in (m, b)
+eval m (Asig x e) =
+    let (m', v) = eval m e
+    in (upd x v m', v)
+eval m (Emp) = (m, IntList [])
+eval m (Uni x) = (m, IntList [x])
+eval m (Bel z e) =
+    let IntList x = snd(eval m e)
+        v = belongs z x
+    in (m, BoolVal v)
+eval m (Join e1 e2) = 
+    let IntList x = snd(eval m e1)
+        IntList y = snd(eval m e2)
+        v = nub(unioni x y)
+    in (m, IntList v)
+eval m (Inter e1 e2) =
+    let IntList x = snd(eval m e1)
+        IntList y = snd(eval m e2)
+        v = nub(intersection x y)
+    in (m, IntList v)
+eval m (Diff e1 e2) =
+    let IntList x = snd(eval m e1)
+        IntList y = snd(eval m e2)
+        v = nub(difference x y)
+    in (m, IntList v)
+eval m (Incl e1 e2) =
+    let IntList x = snd(eval m e1)
+        IntList y = snd(eval m e2)
+        v = included x y
+    in (m, BoolVal v)
+eval m (Pow e) = 
+    let IntList x = snd(eval m e)
+        v = power x
+    in (m, ListIntList v)
+eval m (Equ e1 e2) =
+    let IntList x = snd (eval m e1) 
+        IntList y = snd (eval m e2)
+        v = igual x y
+    in (m, BoolVal v)
+
+
